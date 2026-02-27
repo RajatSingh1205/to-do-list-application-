495ea189934586771bad0cb52b23b2cb7729dbab
ichirokun12 <rajat.k.singh1209@gmail.com>
2026-02-27 18:37:24 +0530

    docker

--- src/main/java/com/example/demo/controller/HealthCheck.java
+++ src/main/java/com/example/demo/controller/HealthCheck.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheck {
 
     @GetMapping
-    public String checkHealth() {
-        return "ok";
+    public ResponseEntity<?> checkHealth() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
